6c70e2f55e5a42d9210c5d67254b4b7d9a542b80
fatemeh-lohrasbi <lohrasbi.developer@gmail.com>
2021-03-29 00:25:17 +0430

    logic 4
    1-style changes when input number is correct
    2-reset game onclick again button

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 //secret number
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess);
+
+
     //when there is no input
     if (!guess) {
         document.querySelector('.message').textContent =
@@ -13,6 +14,9 @@ document.querySelector('.check').addEventListener('click', function () {
         //when player wins
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = 'Correct Number!🎉💃'
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('body').style.backgroundColor='#60b347';
+        document.querySelector('.number').style.width = '30rem';
         //when guess is too heigh
     } else if (guess > secretNumber) {
         if (score > 1) {
@@ -35,3 +39,17 @@ document.querySelector('.check').addEventListener('click', function () {
         }
     }
 });
+
+document.querySelector('.again').addEventListener('click', function(){
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20 + 1);
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('body').style.backgroundColor='#222';
+    document.querySelector('.guess').value ='';
+
+
+});
+
