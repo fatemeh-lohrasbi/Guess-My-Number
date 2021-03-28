c62ee8b4cce569f37c4595ef263a9536ffffb44c
fatemeh-lohrasbi <lohrasbi.developer@gmail.com>
2021-03-28 23:38:50 +0430

    logic 3
    each time that we guess a wrong number, our score decrease.

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 'use strict';
 //secret number
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
-    console.log(guess);    
+    console.log(guess);
     //when there is no input
     if (!guess) {
         document.querySelector('.message').textContent =
             'No number!🤔 ';
+        //when player wins
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = 'Correct Number!🎉💃'
+        //when guess is too heigh
     } else if (guess > secretNumber) {
-        document.querySelector('.message').textContent = 'too high 📈';
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'too high 📈';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game😥';
+            document.querySelector('.score').textContent = 0;
+        }
+        //when guess is too low
     } else if (guess < secretNumber) {
-        document.querySelector('.message').textContent = 'too low 📉';
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'too low 📉';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game😥';
+            document.querySelector('.score').textContent = 0;
+        }
     }
 });
