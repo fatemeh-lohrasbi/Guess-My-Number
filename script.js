0e90ef888b70227d231ed2bc63a8e6adb966470b
fatemeh-lohrasbi <lohrasbi.developer@gmail.com>
2021-03-28 23:12:48 +0430

    logic 2
    define secret number and compare it with input number

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 'use strict';
-
-document.querySelector('.check').addEventListener('click',function(){
-   const guess =Number(document.querySelector('.guess').value);
-   console.log(guess); 
-
-//when there is no input
-   if(!guess){
-       document.querySelector('.message').textContent =
-       "No number!🤔 ";
-   }
+//secret number
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess);    
+    //when there is no input
+    if (!guess) {
+        document.querySelector('.message').textContent =
+            'No number!🤔 ';
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'Correct Number!🎉💃'
+    } else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = 'too high 📈';
+    } else if (guess < secretNumber) {
+        document.querySelector('.message').textContent = 'too low 📉';
+    }
 });
